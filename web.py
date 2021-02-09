427ccab4e54cec824e39f7d91092e70b8196f023
unknown <lenforiee@gmail.com>
2021-02-09 10:29:16 +0100

    dodanie fajnego 404 handlera, przeniesienie homepage do wlasnego handlera

--- web.py
+++ web.py
@@ -5,12 +5,18 @@ app = Quart(__name__)
 __author__ = "Lenforiee"
 __version__ = "1.0.0"
 
+@app.route("/")
+async def home_page():
+    """Render strony głownej."""
+
+    return await render_template('homepage.html')
+
 @app.errorhandler(404)
 async def page_not_found(e):
-    """Our own custom 404 page"""
+    """Nasz własny 404 error handler."""
 
-    return await render_template('homepage.html'), 404
+    return await render_template('404.html'), 404
 
 if __name__ == '__main__':
     os.chdir(os.path.dirname(os.path.realpath(__file__)))
-    app.run(debug=True) # blocking call
+    app.run(debug=True)
